e14cb2034a04c297ec79c07ecc2a532985e0c791
candela99 <gonzalezcandela759@gmail.com>
2023-08-03 12:03:33 -0300

    conexión con base de datos

--- ob-spring-security-cifrado/src/main/java/com/example/obspringsecuritycifrado/ObSpringSecurityCifradoApplication.java
+++ ob-spring-security-cifrado/src/main/java/com/example/obspringsecuritycifrado/ObSpringSecurityCifradoApplication.java
@@ -2,12 +2,25 @@ package com.example.obspringsecuritycifrado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ObSpringSecurityCifradoApplication {
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(ObSpringSecurityCifradoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(ObSpringSecurityCifradoApplication.class, args);
+		UserRepository userRepository = context.getBean(UserRepository.class);
+		PasswordEncoder encoder = context.getBean(PasswordEncoder.class);
+		User user = new User(null, "usuario", encoder.encode("admin"));
+		userRepository.save(user);
 	}
 
 }
